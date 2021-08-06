56f8797153c2feda9d0312bb480931e6d0b0c5bf
Brandon May <battleb2014@gmail.com>
2021-08-05 22:42:51 -0400

    finished

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -18,6 +18,7 @@ const cards = document.querySelector('.cards');
 //     console.error(err);
 //   })
 /*
+
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
     data in order to use it to build your component function
